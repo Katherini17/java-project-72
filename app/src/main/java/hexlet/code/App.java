a0ed90f98f098a13c4e676b27a9dae6fe44a8f21
Ekaterina Avdeenko <Katherini@yandex.ru>
2025-09-25 20:24:09 +0300

    feat: Add support for PORT environment variable

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,6 +3,8 @@ package hexlet.code;
 import io.javalin.Javalin;
 
 public class App {
+    private static final String DEFAULT_PORT = "7070";
+
     public static void main(String[] args) {
         Javalin app = getApp();
 
@@ -10,7 +12,7 @@ public class App {
             ctx.result("Hello World!");
         });
 
-        app.start(7070);
+        app.start(getPort());
     }
 
     public static Javalin getApp() {
@@ -19,5 +21,10 @@ public class App {
         });
 
         return app;
+    }
+
+    public static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", DEFAULT_PORT);
+        return Integer.parseInt(port);
     }
 }
