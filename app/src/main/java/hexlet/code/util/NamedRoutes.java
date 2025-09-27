dcfa6324f23869ae1cf59e4bd4c2df9280782db8
Ekaterina Avdeenko <Katherini@yandex.ru>
2025-09-27 17:02:21 +0300

    feat: add NamedRoutes utility class for paths

---
+++ app/src/main/java/hexlet/code/util/NamedRoutes.java
@@ -0,0 +1,15 @@
+package hexlet.code.util;
+
+public class NamedRoutes {
+    private static final String ROOT_PATH = "/";
+    private static final String URLS_PATH = "/urls";
+
+    public static String rootPath() {
+        return ROOT_PATH;
+    }
+
+    public static String urlsPath() {
+        return URLS_PATH;
+    }
+
+}
